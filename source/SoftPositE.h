eb77b177dfe086a834b850dc0d53746a015d94a1
Qiong Li <qleenju@163.com>
2023-03-26 12:14:47 +0800

    feat: add declaration of convert function

--- source/SoftPositE.h
+++ source/SoftPositE.h
@@ -2,14 +2,22 @@
  * @Author: Qiong Li
  * @Date: 2023-03-17 17:01:56
  * @LastEditors: Qiong Li
- * @LastEditTime: 2023-03-17 17:05:38
- * @FilePath: \posit_tensor_core\SoftPositE\SoftPositE.h
+ * @LastEditTime: 2023-03-26 11:09:51
+ * @FilePath: \SoftPositE\source\SoftPositE.h
  * @Description: 
  * @Reference: 
  * Copyright (c) 2023 by Qiong Li qleenju@163.com, All Rights Reserved. 
  */
+
 #include "include/platform.h"
 #include "include/internals.h"
 
+// posit-based fma unit, supporting arbitrary posit format
 uint32_t posit_muladd(
 	uint_fast32_t uiA, uint_fast32_t uiB, uint_fast32_t uiC, uint_fast32_t op, int n, int es);
+
+// convert arbitrary posit format to double format
+double convertPositToDouble(posit32_t pA, int n, int es);
+
+// convert double format to arbitrary posit format
+posit32_t convertDoubleToPosit(double f32, int n, int es);
