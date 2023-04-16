79fc89d0b75f5543d644e83f60163c3ce5ea922a
Qiong Li <qleenju@163.com>
2023-04-16 18:34:16 +0800

    feat: posit fma unit, supporting mixed-precision

--- source/posit_muladd_mixed.c
+++ source/posit_muladd_mixed.c
@@ -2,7 +2,7 @@
  * @Author: Qiong Li
  * @Date: 2023-04-16 14:05:18
  * @LastEditors: Qiong Li
- * @LastEditTime: 2023-04-16 14:40:27
+ * @LastEditTime: 2023-04-16 15:02:03
  * @FilePath: \SoftPositE\source\posit_muladd_mixed.c
  * @Description: Posit-based FMA function implemented by C, supporting mixed-precision strategy
  *      - Support arbitrary posit formats, i.e., posit(n, es)
@@ -15,8 +15,9 @@
 #include "SoftPositE.h"
 
 uint32_t
-posit_muladd(
-	uint_fast32_t uiA, uint_fast32_t uiB, uint_fast32_t uiC, uint_fast32_t op, int n, int es) {
+posit_muladd_mixed(
+	uint_fast32_t uiA, uint_fast32_t uiB, uint_fast32_t uiC, uint_fast32_t op, 
+    int n_i, int es_i, int n_o, int es_o) {
 
 	uint32_t uZ;
 	int regZ;
@@ -25,21 +26,25 @@ posit_muladd(
 	int_fast32_t expA, expC, expZ;
 	int_fast16_t kA = 0, kC = 0, kZ = 0, shiftRight;
 	uint_fast64_t frac64C, frac64Z;
+	// valid exponent of product of A & B
+	int_fast32_t rg_exp_AB;
 	// the maximum value that can be represented by es-bit exponent field
 	// Note: subtraction has higher priority than left shift
-	int exp_max = (0x00000001<<es) - 1; 
+	int exp_max_i = (0x00000001<<es_i) - 1;
+    int exp_max_o = (0x00000001<<es_o) - 1;
 	int count;
 
 	// Ensure 2<=n<=32
-	if (n < 2 || n>32) {
+	if (n_i<2 || n_i>32 || n_o<2 || n_o>32) {
 		uZ = 0x80000000;
 		return uZ;
 	}
 
 	// Set the LSB (32-n)-bits 0
-	uiA = uiA & (0xffffffff << (32 - n));
-	uiB = uiB & (0xffffffff << (32 - n));
-	uiC = uiC & (0xffffffff << (32 - n));
+	uiA = uiA & (0xffffffff << (32 - n_i));
+	uiB = uiB & (0xffffffff << (32 - n_i));
+    // n_o for uiC, instread of n_i
+	uiC = uiC & (0xffffffff << (32 - n_o));
 
 	// NaR Handing
 	if (uiA == 0x80000000 || uiB == 0x80000000 || uiC == 0x80000000) {
@@ -77,7 +82,8 @@ posit_muladd(
 	regSB = signregP32UI(uiB);
 	regSC = signregP32UI(uiC);
 
-	if (n == 2) {
+    // Actually there is no need to apply 2-bit posit format?
+	if (n_i == 2) {
 		// if regSA=1 && regSB=1, then |a|=|b|=1
 		// if regSA=0 or regSB=0, then the value is 0 or NaN (return)
 		uZ = (regSA & regSB) ? (0x40000000) : (0x0);
@@ -117,7 +123,7 @@ posit_muladd(
 		count = 0;
 		// regSA=1, means the regime field starts with consecutive 1
 		if (regSA) {
-			while (tmp >> 31 && count<(n-2)) {
+			while (tmp >> 31 && count<(n_i-2)) {
 				// the initial value of KA is 0
 				kA++;
 				tmp = (tmp << 1) & 0xFFFFFFFF;
@@ -126,7 +132,7 @@ posit_muladd(
 		}
 		else {
 			kA = -1;
-			while (!(tmp >> 31) && count<n-2) {
+			while (!(tmp >> 31) && count<n_i-2) {
 				kA--;
 				tmp = (tmp << 1) & 0xFFFFFFFF;
 				count++;
@@ -135,15 +141,15 @@ posit_muladd(
 		}
 		// The "tmp" obtained above is 32-bit, where the MSB bit is the the LSB-bit of regime field, and is forced to 0.
 		// The exponent field start from the 2nd bit, with a bit-width of es
-		expA = tmp >> (31-es);
+		expA = tmp >> (31-es_i);
 		// The MSB bit of 32-bit fracA is implicit bit, and is forced to 1 (since effective zero has been handled in special cases)
-		fracA = ((tmp << es) | 0x80000000) & 0xFFFFFFFF;
+		fracA = ((tmp << es_i) | 0x80000000) & 0xFFFFFFFF;
 
 		// Handle regime field of uiB, identical to the process of uiA
 		tmp = (uiB << 2) & 0xFFFFFFFF;
 		count = 0;
 		if (regSB) {
-			while (tmp >> 31 && count<n-2) {
+			while (tmp >> 31 && count<n_i-2) {
 				kA++;
 				tmp = (tmp << 1) & 0xFFFFFFFF;
 				count++;
@@ -151,7 +157,7 @@ posit_muladd(
 		}
 		else {
 			kA--;
-			while (!(tmp >> 31) && count<n-2) {
+			while (!(tmp >> 31) && count<n_i-2) {
 				kA--;
 				tmp = (tmp << 1) & 0xFFFFFFFF;
 				count++;
@@ -159,24 +165,23 @@ posit_muladd(
 			tmp &= 0x7FFFFFFF;
 		}
 		// addition of exponents of a, b
-		expA += tmp >> (31-es);
-		fracB = ((tmp << es) | 0x80000000) & 0xFFFFFFFF;
+		expA += tmp >> (31-es_i);
+		fracB = ((tmp << es_i) | 0x80000000) & 0xFFFFFFFF;
 		// multiplication of mantissa of a, b (include implicit bit), to obtain 64-bit frac64Z
 		frac64Z = (uint_fast64_t)fracA * fracB;
 
-		// if exponent overflows, it should be rounded to regime field, where the scale factor is 2^es, i.e., exp_max+1
-		if (expA > exp_max) {
-			kA++;
-			expA -= (exp_max+1);
-		}
+		// Mixed-Precision Strategy, (n_i, es_i) --> (n_o, es_o)
+		rg_exp_AB = (kA<<es_i) + expA;
+		kA = rg_exp_AB >> es_o;
+		expA = rg_exp_AB & (~(0xFFFFFFFF<<es_o));
 
 		rcarry = frac64Z >> 63;
 		// 1-bit normalization
 		if (rcarry) {
 			expA++;
-			if (expA > exp_max) {
+			if (expA > exp_max_o) {
 				kA++;
-				expA -=  (exp_max+1);
+				expA -= (exp_max_o+1);
 			}
 			// keep the decimal point at the 2nd bit, and now the MSB-bit of frac64Z is 0
 			frac64Z >>= 1;
@@ -186,7 +191,7 @@ posit_muladd(
 			tmp = (uiC << 2) & 0xFFFFFFFF;
 			count = 0;
 			if (regSC) {
-				while (tmp >> 31 && count<=n-es-1) {
+				while (tmp >> 31 && count<=n_o-2) {
 					kC++;
 					tmp = (tmp << 1) & 0xFFFFFFFF;
 					count++;
@@ -194,7 +199,7 @@ posit_muladd(
 			}
 			else {
 				kC = -1;
-				while (!(tmp >> 31) && count<=n-es-1) {
+				while (!(tmp >> 31) && count<=n_o-2) {
 					kC--;
 					tmp = (tmp << 1) & 0xFFFFFFFF;
 					count++;
@@ -202,15 +207,15 @@ posit_muladd(
 				tmp &= 0x7FFFFFFF;
 			}
 
-			expC = tmp >> (31-es);
+			expC = tmp >> (31-es_o);
 			// Also keep the decimal point of frac64C at the 2nd bit
-			if (es > 0)
-				frac64C = (((tmp << (es - 1)) | 0x40000000ULL) & 0x7FFFFFFFULL) << 32;
+			if (es_o > 0)
+				frac64C = (((tmp << (es_o - 1)) | 0x40000000ULL) & 0x7FFFFFFFULL) << 32;
 			else
 				frac64C = (((tmp >> 1) | 0x40000000ULL) & 0x7FFFFFFFULL) << 32;
 			
 			// the exponent difference of ab and c, i.e., e_ab - e_c
-			shiftRight = ((kA - kC) << es) + (expA - expC);
+			shiftRight = ((kA - kC) << es_o) + (expA - expC);
 
 			// |uiC| > |prod|, right shift frac64Z
 			if (shiftRight < 0) {
@@ -283,9 +288,9 @@ posit_muladd(
 			// 1-bit normalization
 			if (rcarry) {
 				expZ++;
-				if (expZ > exp_max) {
+				if (expZ > exp_max_o) {
 					kZ++;
-					expZ -= (exp_max+1);
+					expZ -= (exp_max_o+1);
 				}
 				if (frac64Z & 0x1)
 					bitsMore = 1;
@@ -296,16 +301,16 @@ posit_muladd(
 				if (frac64Z != 0) {
 					// e.g., es=1 --> exp_max=1 --> frac64Z>>61, leave the MSB 3-bits
 					// Since the MSB bit has been 0, so if the remaining 2-bits are 0, it can be normalized
-					while ((frac64Z >> (62-exp_max)) == 0) {
+					while ((frac64Z >> (62-exp_max_o)) == 0) {
 						kZ--;
-						frac64Z <<= (exp_max+1);
+						frac64Z <<= (exp_max_o+1);
 					}
 					while ((frac64Z >> 62) == 0) {
 						expZ--;
 						frac64Z <<= 1;
 						if (expZ < 0) {
 							kZ--;
-							expZ = exp_max;
+							expZ = exp_max_o;
 						}
 					}
 				}
@@ -333,9 +338,9 @@ posit_muladd(
 			regime = 0x7FFFFFFF - (0x7FFFFFFF >> regZ);
 		}
 
-		if (regZ > (n - 2)) {
+		if (regZ > (n_o - 2)) {
 			// max or min posit number. exp and frac does not matter.
-			uZ = (regSZ) ? (0x7FFFFFFF & ((int32_t)0x80000000 >> (n - 1))) : (0x1 << (32 - n));
+			uZ = (regSZ) ? (0x7FFFFFFF & ((int32_t)0x80000000 >> (n_o - 1))) : (0x1 << (32 - n_o));
 		}
 		else {
 			// remove hidden bits
@@ -343,41 +348,41 @@ posit_muladd(
 			// the total bit-width of sign & regime & exponent is 1+(regZ+1) + es, i.e., (regZ+es+2)
 			// Note: the MSB 2-bit of frac64Z has been set to 0
 			// So, after the following shift, the MSB (regZ+es+2)-bits are forced to 0, to save sign & regime & exponent fields
-			fracZ = frac64Z >> (regZ + es + 32);
+			fracZ = frac64Z >> (regZ + es_o + 32);
 
 			// The shifted bits all come from mantissa field
-			if (regZ <= (n - es-2)) {
-				bitNPlusOne = (((uint64_t)0x8000000000000000 >> (n - regZ - es)) & frac64Z);
-				bitsMore |= (((uint64_t)0x7FFFFFFFFFFFFFFF >> (n - regZ - es)) & frac64Z);
+			if (regZ <= (n_o-es_o-2)) {
+				bitNPlusOne = (((uint64_t)0x8000000000000000 >> (n_o - regZ - es_o)) & frac64Z);
+				bitsMore |= (((uint64_t)0x7FFFFFFFFFFFFFFF >> (n_o - regZ - es_o)) & frac64Z);
 				// set the LSB (32-n) bits to 0 (since the valid bit-width is n)
-				fracZ &= ((int32_t)0x80000000 >> (n - 1));
-				expZ <<= (30 - es - regZ);	
+				fracZ &= ((int32_t)0x80000000 >> (n_o - 1));
+				expZ <<= (30 - es_o - regZ);	
 			}
 			else {
 				// (n-es-1) <= regZ <= (n-2), partially the shifted bits come from exponent field
-				if (regZ - n + es + 1 > 0)
-					bitNPlusOne = expZ & (0x00000001 << (regZ - n + es + 1));
+				if (regZ - n_o + es_o + 1 > 0)
+					bitNPlusOne = expZ & (0x00000001 << (regZ - n_o + es_o + 1));
 				else
 					bitNPlusOne = expZ & 0x1;
 					
-				if (regZ - n + es > 0)
+				if (regZ - n_o + es_o > 0)
 					// bitsMore = expZ & (0x00000001 << (regZ - n + es));
-					bitsMore |= expZ & ~(0xFFFFFFFF<<(regZ-n+es+1));
-				else if (regZ - n + es == 0)
+					bitsMore |= expZ & ~(0xFFFFFFFF<<(regZ-n_o+es_o+1));
+				else if (regZ - n_o + es_o == 0)
 					bitsMore |= expZ & 0x1;
 				if (frac64Z > 0) {
 					fracZ = 0;
 					bitsMore = 1;
 				}
 
-				expZ = expZ >> (es + 2 - n + regZ);
-				expZ = expZ << (32 - n);
+				expZ = expZ >> (es_o + 2 - n_o + regZ);
+				expZ = expZ << (32 - n_o);
 			}
 
 			uZ = packToP32UI(regime, expZ, fracZ);
 
  			if (bitNPlusOne) {
-				uZ += (uint32_t)(((uZ >> (32 - n)) & 1) | bitsMore) << (32 - n);
+				uZ += (uint32_t)(((uZ >> (32 - n_o)) & 1) | bitsMore) << (32 - n_o);
 			}
 
 		}
