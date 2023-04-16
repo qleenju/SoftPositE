cacffe83e47a9b9994e5b9d50769f691a2b6a954
Qiong Li <qleenju@163.com>
2023-04-16 18:35:31 +0800

    test: add test_posit_muladd_mixed()

--- source/test/main.c
+++ source/test/main.c
@@ -2,21 +2,49 @@
  * @Author: Qiong Li
  * @Date: 2023-03-18 12:28:20
  * @LastEditors: Qiong Li
- * @LastEditTime: 2023-03-26 12:12:00
+ * @LastEditTime: 2023-04-16 16:20:59
  * @FilePath: \SoftPositE\source\test\main.c
  * @Description: 
  * @Reference: 
  * Copyright (c) 2023 by Qiong Li qleenju@163.com, All Rights Reserved. 
  */
+
 #include <stdio.h>
 #include "../SoftPositE.h"
 
 void test_posit_muladd();
 void test_convertPositToDouble();
+void test_posit_muladd_mixed();
 
 int main(){
     // test_posit_muladd();
-    test_convertPositToDouble();
+    // test_convertPositToDouble();
+    test_posit_muladd_mixed();
+    
+}
+
+void test_posit_muladd_mixed(){
+    uint32_t uiZ;
+    uint_fast32_t uiA, uiB, uiC;
+    uint_fast32_t op;
+    int n_i, es_i;
+    int n_o, es_o;
+
+    // test instances
+    n_i = 8;
+    es_i = 3;
+    n_o = 8;
+    es_o = 3;
+    uiA = 0x5a << (32-n_i);
+    uiB = 0x64 << (32-n_i);
+    uiC = 0x01 << (32-n_o);
+    op = 0;
+    
+    uiZ = posit_muladd_mixed(uiA, uiB, uiC, op, n_i, es_i, n_o, es_o);
+    uiZ = uiZ >> (32-n_o);
+    printf("uiZ = ");
+    printBinary((uint64_t*)&uiZ, n_o);
+    
 }
 
 void test_posit_muladd(){
